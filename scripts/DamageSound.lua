8ca4b1ad8654d65d6790243b804800959e0c7351
Total <39064108+TotalTakeover@users.noreply.github.com>
2026-06-23 08:09:04 -0700

    Hurt sound
    - Copied and tweaked from my other templates

---
+++ scripts/DamageSound.lua
@@ -0,0 +1,17 @@
+function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
+	
+	-- Don't trigger if the sound was played by Figura (prevent potential infinite loop)
+	if not path then return end
+	
+	-- Don't do anything if the user isn't loaded
+	if not player:isLoaded() then return end
+	
+	-- Make sure the sound is (most likely) played by the user
+	if (player:getPos() - pos):length() > 0.05 then return end
+	
+	-- If sound contains ".hurt", play an additional hurt sound along side it
+	if id:find(".hurt") then
+		sounds:playSound("entity.turtle.hurt", pos, 0.75, math.random() + 0.5)
+	end
+	
+end
