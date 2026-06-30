8750c2a5465415a1e14d689546a54e020f94ce1a
Total <39064108+TotalTakeover@users.noreply.github.com>
2026-06-29 22:33:59 -0700

    Armor support
    - Ported armor from another avatar of mine.
      - I do not have enough time to work on making armor textures, but framework is nice to have, as I plan on eventually making some for this avatar.

---
+++ scripts/Armor.lua
@@ -0,0 +1,264 @@
+-- Required scripts
+local parts       = require("lib.PartsAPI")
+local turtleArmor = require("lib.KattArmor")()
+local sync        = require("lib.LetThatSyncFig")
+
+-- Synced variables setup
+local helmet     = sync.new("ArmorHelmet", true):config()
+local chestplate = sync.new("ArmorChestplate", true):config()
+local leggings   = sync.new("ArmorLeggings", true):config()
+local boots      = sync.new("ArmorBoots", true):config()
+
+-- Setting the leggings to layer 1
+turtleArmor.Armor.Leggings:setLayer(1)
+
+-- Armor parts
+turtleArmor.Armor.Leggings
+	:addParts(table.unpack(parts:createTable(function(part) return part:getName() == "Leggings" end)))
+	:addTrimParts(table.unpack(parts:createTable(function(part) return part:getName() == "LeggingsTrim" end)))
+turtleArmor.Armor.Boots
+	:addParts(table.unpack(parts:createTable(function(part) return part:getName() == "Boot" end)))
+	:addTrimParts(table.unpack(parts:createTable(function(part) return part:getName() == "BootTrim" end)))
+
+-- Leather armor
+turtleArmor.Materials.leather
+	:setTexture(textures["textures.armor.leatherArmor"] or textures["Turtle.leatherArmor"])
+	:addParts(turtleArmor.Armor.Leggings, table.unpack(parts:createTable(function(part) return part:getName() == "LeggingsLeather" end)))
+	:addParts(turtleArmor.Armor.Boots,    table.unpack(parts:createTable(function(part) return part:getName() == "BootLeather" end)))
+
+-- Chainmail armor
+turtleArmor.Materials.chainmail
+	:setTexture(textures["textures.armor.chainmailArmor"] or textures["Turtle.chainmailArmor"])
+
+-- Iron armor
+turtleArmor.Materials.iron
+	:setTexture(textures["textures.armor.ironArmor"] or textures["Turtle.ironArmor"])
+
+-- Golden armor
+turtleArmor.Materials.golden
+	:setTexture(textures["textures.armor.goldenArmor"] or textures["Turtle.goldenArmor"])
+
+-- Diamond armor
+turtleArmor.Materials.diamond
+	:setTexture(textures["textures.armor.diamondArmor"] or textures["Turtle.diamondArmor"])
+
+-- Netherite armor
+turtleArmor.Materials.netherite
+	:setTexture(textures["textures.armor.netheriteArmor"] or textures["Turtle.netheriteArmor"])
+
+-- Trims
+local trims = {
+	"bolt",
+	"coast",
+	"dune",
+	"eye",
+	"flow",
+	"host",
+	"raiser",
+	"rib",
+	"sentry",
+	"shaper",
+	"silence",
+	"snout",
+	"spire",
+	"tide",
+	"vex",
+	"ward",
+	"wayfinder",
+	"wild"
+}
+
+-- Apply trims
+for _, trim in ipairs(trims) do
+	local tex = textures["textures.armor.trims."..trim.."Trim"] or textures["Turtle."..trim.."Trim"] or false
+	if tex then
+		turtleArmor.TrimPatterns[trim]:setTexture(tex)
+	end
+end
+
+-- Helmet parts
+local helmetGroups = {
+	
+	vanilla_model.HELMET
+	
+}
+
+-- Chestplate parts
+local chestplateGroups = {
+	
+	vanilla_model.CHESTPLATE
+	
+}
+
+-- Leggings parts
+local leggingsGroups = {
+	
+	table.unpack(parts:createTable(function(part) return part:getName():find("ArmorLeggings") end))
+	
+}
+
+-- Boots parts
+local bootsGroups = {
+	
+	table.unpack(parts:createTable(function(part) return part:getName():find("ArmorBoot") end))
+	
+}
+
+function events.RENDER(delta, context)
+	
+	-- Apply
+	for _, part in ipairs(helmetGroups) do
+		part:visible(helmet.curr)
+	end
+	
+	for _, part in ipairs(chestplateGroups) do
+		part:visible(chestplate.curr)
+	end
+	
+	for _, part in ipairs(leggingsGroups) do
+		part:visible(leggings.curr)
+	end
+	
+	for _, part in ipairs(bootsGroups) do
+		part:visible(boots.curr)
+	end
+	
+end
+
+-- Play sound if toggling armor
+local function equipSound()
+	if player:isLoaded() then
+		sounds:playSound("item.armor.equip_generic", player:getPos(), 0.5)
+	end
+end
+
+-- Apply sound to sync updates
+helmet:applyFunc(equipSound)
+chestplate:applyFunc(equipSound)
+leggings:applyFunc(equipSound)
+boots:applyFunc(equipSound)
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Required scripts
+local s, wheel, c = pcall(require, "scripts.ActionWheel")
+if not s then return end -- Kills script early if ActionWheel.lua isnt found
+pcall(require, "scripts.Player") -- Tries to find script, not required
+
+-- Pages
+local parentPage = action_wheel:getPage("Player") or action_wheel:getPage("Main")
+local armorPage  = action_wheel:newPage("Armor")
+
+-- Actions table setup
+local a = {}
+
+-- Actions
+a.pageAct = parentPage:newAction()
+	:item("iron_chestplate")
+	:onLeftClick(function() wheel:descend(armorPage) end)
+
+a.allAct = armorPage:newAction()
+	:item("armor_stand")
+	:toggleItem("netherite_chestplate")
+	:onToggle(function(bool)
+		helmet:update(bool)
+		chestplate:update(bool)
+		leggings:update(bool)
+		boots:update(bool)
+	end)
+
+a.helmetAct = armorPage:newAction()
+	:item("iron_helmet")
+	:toggleItem("diamond_helmet")
+	:onToggle(function(bool)
+		helmet:update(bool)
+	end)
+
+a.chestplateAct = armorPage:newAction()
+	:item("iron_chestplate")
+	:toggleItem("diamond_chestplate")
+	:onToggle(function(bool)
+		chestplate:update(bool)
+	end)
+
+a.leggingsAct = armorPage:newAction()
+	:item("iron_leggings")
+	:toggleItem("diamond_leggings")
+	:onToggle(function(bool)
+		leggings:update(bool)
+	end)
+
+a.bootsAct = armorPage:newAction()
+	:item("iron_boots")
+	:toggleItem("diamond_boots")
+	:onToggle(function(bool)
+		boots:update(bool)
+	end)
+
+-- Update actions
+function events.RENDER(delta, context)
+	
+	if action_wheel:isEnabled() then
+		a.pageAct
+			:title(toJson(
+				{text = "Armor Settings", bold = true, color = c.primary}
+			))
+		
+		a.allAct
+			:title(toJson(
+				{
+					"",
+					{text = "Toggle All Armor\n\n", bold = true, color = c.primary},
+					{text = "Toggles visibility of all armor parts.", color = c.secondary}
+				}
+			))
+			:toggled(helmet.curr and chestplate.curr and leggings.curr and boots.curr)
+		
+		a.helmetAct
+			:title(toJson(
+				{
+					"",
+					{text = "Toggle Helmet\n\n", bold = true, color = c.primary},
+					{text = "Toggles visibility of helmet parts.", color = c.secondary}
+				}
+			))
+			:toggled(helmet.curr)
+		
+		a.chestplateAct
+			:title(toJson(
+				{
+					"",
+					{text = "Toggle Chestplate\n\n", bold = true, color = c.primary},
+					{text = "Toggles visibility of chestplate parts.", color = c.secondary}
+				}
+			))
+			:toggled(chestplate.curr)
+		
+		a.leggingsAct
+			:title(toJson(
+				{
+					"",
+					{text = "Toggle Leggings\n\n", bold = true, color = c.primary},
+					{text = "Toggles visibility of leggings parts.", color = c.secondary}
+				}
+			))
+			:toggled(leggings.curr)
+		
+		a.bootsAct
+			:title(toJson(
+				{
+					"",
+					{text = "Toggle Boots\n\n", bold = true, color = c.primary},
+					{text = "Toggles visibility of boots.", color = c.secondary}
+				}
+			))
+			:toggled(boots.curr)
+		
+		for _, act in pairs(a) do
+			act:hoverColor(c.hover):toggleColor(c.active)
+		end
+		
+	end
+	
+end
