f622cc28aff8812e73ae187ba16c96142af85ff1
Total <39064108+TotalTakeover@users.noreply.github.com>
2026-08-06 00:22:39 -0700

    Missing delta
    - Forget to add the `delta` arg to `getOriginRot()` clamper.

--- scripts/Anims.lua
+++ scripts/Anims.lua
@@ -54,8 +54,8 @@ local leftArmLerp  = lerp.new(armsMove.curr and 1 or 0, 0.5)
 local rightArmLerp = lerp.new(armsMove.curr and 1 or 0, 0.5)
 
 -- Gets the origin rotation of a part, clamped
-local function getOriginRot(part)
-	return (vanilla_model[part]:getOriginRot() + 180) % 360 - 180
+local function getOriginRot(part, delta)
+	return (vanilla_model[part]:getOriginRot(delta) + 180) % 360 - 180
 end
 
 -- Parrot pivots
